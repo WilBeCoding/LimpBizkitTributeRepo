295dfa1d388880769a41024b683ad9b690227c18
Wilson Rondini <wilbecoding@gmail.com>
2015-11-12 16:37:40 -0700

    Route changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = "postgres://localhost/memoriesapp";
+var conString = process.env.DATABASE_URL || "postgres://@localhost/memoriesapp";
 
 /* GET home page. */
+router.get('/api/v1/memories', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * from memories', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected to db')
+    });
+  });
+});
+
 router.post('/api/v1/memories', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
@@ -11,7 +26,6 @@ router.post('/api/v1/memories', function(req, res, next) {
     }
     client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) ', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
       done();
-      res.render('index');
       if (err) {
         return console.error('error running query', err);
       }
